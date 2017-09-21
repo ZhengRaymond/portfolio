f883f7811ca747e02009ebb9c7464aecdf51940f
ZhengRaymond <raymond.zheng@edu.uwaterloo.ca>
2017-09-21 03:55:10 -0400

    Added scrolling navigation.

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -7,6 +7,7 @@ import { fetch_works, fetch_projects } from 'actions';
 
 import { fadein, fadein2, fadein3, glow } from 'styles/animations';
 import COLORS from 'styles/colors';
+import Scroll, { Link, Element } from 'react-scroll';
 
 import Page from 'containers/Page';
 import Header from 'components/header'
@@ -19,7 +20,8 @@ const ColorText = styled.span`
 `
 
 const DownwardArrow = styled(FADown)`
-  position: relative;
+  position: absolute;
+  top: 90vh;
   text-align: center;
 
   opacity: 0.0;
@@ -84,7 +86,7 @@ const OnePage = styled.div`
 `
 
 const About = styled.div`
-  width: 70vw;
+  width: 60vw;
   padding: 20px 0 15vh 0;
   text-align: left;
   font-size: 1em;
@@ -136,35 +138,122 @@ const data = (state = { value: 0 }, action) => {
   }
 }
 
-const ScrollNav = styled.div`
+const ScrollNavArea = styled.div`
   position: fixed;
-  height: 90vh;
-  width: 20px;
-  border-radius: 10px;
-  margin: 5vh;
-  background-color: rgba(255, 255, 255, 0.8);
+  height: 100vh;
+  padding: 5vh 5vh 5vh 3vh;
+  z-index: 3;
+  opacity: 0.5;
+  &:hover {
+    > * {
+      opacity: 1.0;
+      transition: all 0.5s;
+    }
+  }
 `
 
-// const ScrollBall = styled.svg`
-//   position: fixed;
-//   left: 5vh;
-//   width: 20px;
-//   height: 20px;
-//   border-radius: 10px;
-//   background-color: rgba(0, 0, 0, 0.7);
-// `
+const ScrollNav = styled.div`
+  position: absolute;
+  height: 90vh;
+  width: 4px;
+  border-radius: 2px;
+  background-color: rgba(150, 150, 150, 0.8);
+  opacity: 0.5;
+  transition: all 0.5s;
+`
 
-const ScrollBall = () => (
-  <svg style={{
-    position: "fixed",
-    left: "5vh",
-    width: "40px",
-    height: "60px"
-  }}>
-    <path d="M0 0 L0 40 L18 40 L 18 20 L 20 20 L 20 35 L 35 20 L 20 5 L 20 20 L 18 20 L 18 0 L 0 0" stroke="black" fill="black"/>
-  </svg>
-)
-// <path d="M0 0 L0 40 C 18 40, 18 24, 27 20 C 18 16, 18 0, 0 0" stroke="black" fill="black"/>
+const ScrollSocketArea = styled.div`
+  position: relative;
+  margin-left: -18px;
+  margin-top: 12vh;
+  margin-bottom: 25vh;
+  width: 40px;
+  height: 60px;
+  border-radius: 20px;
+
+  &:hover {
+    .socket {
+      transform: scale(3);
+      opacity: 0.8;
+      transition: all 0.5s;
+    }
+    .socket-label {
+      transform: translateX(0px);
+      opacity: 1;
+      transition: all 0.5s;
+    }
+  }
+`;
+
+const ScrollSocketArea2 = styled(Link)`
+  position: absolute;
+  width: 50px;
+  height: 10px;
+  margin-left: -5px;
+  margin-top: 26px;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  cursor: pointer;
+  text-decoration: none;
+  color: black;
+  transition: all 0.5s;
+`;
+
+
+const ScrollSocketBead = styled.div`
+  position: absolute;
+  width: 8px;
+  height: 8px;
+  border-radius: 4px;
+  background-color: black;
+  opacity: 0.5;
+  cursor: pointer;
+  transition: all 0.5s;
+`
+
+const ScrollSocketLabel = styled.div`
+  position: absolute;
+  white-space: nowrap;
+  left: 45px;
+  transform: translateX(-30px);
+  font-size: 15px;
+  opacity: 0;
+  cursor: pointer;
+  transition: all 0.5s;
+`
+
+class ScrollFn extends React.Component {
+  render() {
+    return (
+      <ScrollNavArea className="nav-area">
+        <ScrollNav className="nav">
+          <ScrollSocketArea className="socket-area">
+            <ScrollSocketArea2 to="home" smooth={true} duration={500}>
+              <ScrollSocketBead className="socket"/>
+              <ScrollSocketLabel className="socket-label">Home</ScrollSocketLabel>
+            </ScrollSocketArea2>
+          </ScrollSocketArea>
+          <ScrollSocketArea className="socket-area">
+            <ScrollSocketArea2 to="work-experience" smooth={true} duration={500}>
+              <ScrollSocketBead className="socket"/>
+              <ScrollSocketLabel className="socket-label">Work Experience</ScrollSocketLabel>
+            </ScrollSocketArea2>
+          </ScrollSocketArea>
+          <ScrollSocketArea className="socket-area">
+            <ScrollSocketArea2 to="projects" smooth={true} duration={500} >
+              <ScrollSocketBead className="socket"/>
+              <ScrollSocketLabel className="socket-label">Projects</ScrollSocketLabel>
+            </ScrollSocketArea2>
+          </ScrollSocketArea>
+        </ScrollNav>
+      </ScrollNavArea>
+    )
+  }
+}
 
 class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
@@ -192,31 +281,39 @@ class HomePage extends React.PureComponent { // eslint-disable-line react/prefer
     setTimeout(this.updateBackground, 0);
   }
 
+
+  // <ScrollNav>
+  //   <ScrollSocketFn/>
+  // </ScrollNav>
   render() {
     // But most of all, I am a <ColorText color="green">Learner</ColorText>. The biggest dilemma I experience day-to-day, is my evergrowing bucket-list of things to do, be it learning some new language or taking another interesting course, to scuba-diving (well, free diving) and archery. Alas, if only the 24-hour day was longer...
     return (
       <div>
         <Shelf />
-        <ScrollNav />
-        <ScrollBall />
+        <ScrollFn/>
         <Parallax>
+          <Element name="home"/>
           <br/><br/>
           <OnePage>
             <Header animation={true}>{"Hello, I'm Raymond"}</Header>
             <About>
-              I'm a 3rd-year student at the <b>University of Waterloo</b> (GPA 3.4), pursuing a Bachelor's of Computer Science along with minors in Statistics and in Combinatorics and optimization, expecting to graduate in April 2020.
+              I'm a 3rd-year student at the <ColorText color="gold">University of Waterloo</ColorText> (GPA 3.4), pursuing a Bachelor's of Computer Science along with minors in Statistics and in Combinatorics and optimization, expecting to graduate in April 2020.
               <br/><br/>
               I'm a strong <ColorText color="blue">Full-stack Web Developer</ColorText>, good and experienced with and experienced with platform infrastructure tools. I love solving backend logic, but at the same time have a passion for design. I'm also very interested in <ColorText color="red">Data Science</ColorText>, pursuing the University of Waterloo's Computer Science with Data Science Program.
               <br/><br/>
-              Personally, I'm pursuing a skydiving license, love to go on road trips, hikes, or any outdoor hangouts, and play board games.
+              Personally, I'm pursuing a skydiving license, love to go on road trips, hikes, or any outdoor hangouts, and play board games. I love to read books and watch anime -- always down for a good story!
             </About>
             <DownwardArrow />
             <br/><br/><br/><br/>
           </OnePage>
           <OnePage>
+            <Element name="work-experience" className="element"/>
+            <br/><br/>
             <Page data={this.props.works} title="Work Experience" />
           </OnePage>
           <OnePage>
+            <Element name="projects"/>
+            <br/><br/>
             <Page data={this.props.projects} title="Projects" />
           </OnePage>
           <ParallaxBack>
--- app/styles/colors.js
+++ app/styles/colors.js
@@ -1,7 +1,8 @@
 const COLORS = {
   blue: '#166fff',
   red: '#ff3a28',
-  green: '#00d129'
+  green: '#00d129',
+  gold: '#efb600'
 }
 
 export default COLORS;
